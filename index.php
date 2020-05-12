c3ccd0c343d8df9b1418e7bcde2fc1bb00812e8f
olarte97 <37113938+olarte97@users.noreply.github.com>
2020-05-12 09:58:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -41,8 +41,7 @@
       
       <li class="nav-item">
         <a class="nav-link" href="#">Busqueda</a>
-      </li>þ
-
+      </li>
       
       <li class="nav-item">
         <a class="nav-link" href="#">Acerca de</a>
