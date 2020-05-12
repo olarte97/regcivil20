20ac0f11a7446cbcc408cfa4f0f75329020585b3
olarte97 <37113938+olarte97@users.noreply.github.com>
2020-05-12 15:51:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark ">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Registro</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -56,27 +56,7 @@
 
 <div class="view" style="background-image: url('oficina.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
 
-<!-- Mask & flexbox options-->
-            <div class="mask rgba-black-light align-items-center">
-              <!-- Content -->
-              <div class="container">
-                <!--Grid row-->
-                <div class="row">
-                  <!--Grid column-->
-                  <div class="col-md-12 mb-4 white-text text-center">
-                    <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><strong>Minimalist intro</strong></h1>
-                    <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
-                    <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Photography & design</strong></h5>
-                    <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">portfolio</a>
-                    <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">About me</a>
-                  </div>
-                  <!--Grid column-->
-                </div>
-                <!--Grid row-->
-              </div>
-              <!-- Content -->
-            </div>
-            <!-- Mask & flexbox options-->
+
 </div>
 
 
