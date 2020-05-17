24ca374a3a2efc6d229e8b4b66749dbdcc2e5832
olarte97 <37113938+olarte97@users.noreply.github.com>
2020-05-17 14:07:29 -0500

    Update formNacimiento.php

--- formNacimiento.php
+++ formNacimiento.php
@@ -3,7 +3,7 @@
 
     <div class="form-row">
         <!--row-->
-        <div class="form-group col-md-4">
+        <div class="form-group col-md-2">
             <label for="Check-in">Fecha</label>
             <input type="text" readonly="" class="form-control" id="checkin" placeholder="Check-in">
         </div>
