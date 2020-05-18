bef16c10e0a794f0a5f887323646988ce94a79c8
olarte7 <olarte97@gmail.com>
2020-05-18 12:49:10 -0500

    mjsgs3

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 
 <!-- Modal Nacimiento -->
 <div class="modal fade" id="modalNacimiento" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered " role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Nacimiento</h5>
